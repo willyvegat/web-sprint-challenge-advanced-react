9336f87d1464dce6d926eb50b80d744746ccf3fc
Willy Vega <willy.vegaa@Willys-MBP-2.lan>
2023-01-25 15:46:45 -0500

    reset button, up button is linked and email input works as well

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+
 
 // Suggested initial states
 const initialMessage = ''
@@ -16,6 +17,16 @@ const initialState = {
 export default class AppClass extends React.Component {
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
+  state = {
+      email: '',
+      message: '',
+      totalSteps: 0,
+      movesX: 2,
+      movesY: 2,
+      board: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+      currentIndex: 4
+    }
+
 
   getXY = () => {
     // It it not necessary to have a state to track the coordinates.
@@ -30,25 +41,40 @@ export default class AppClass extends React.Component {
 
   reset = () => {
     // Use this helper to reset all states to their initial values.
+    // console.log('clicked reset');
+    this.setState(initialState)
   }
 
   getNextIndex = (direction) => {
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
+    if (direction === 'up') console.log('up')
+    //   this.setState({...this.state,
+    //   movesY: this.state.movesY - 1
+    // })
+    // }
   }
 
   move = (evt) => {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
+    
   }
 
   onChange = (evt) => {
     // You will need this to update the value of the input.
+    const { value } = evt.target;
+    // console.log(value)
+    this.setState({
+      ...this.state,
+      email: value
+    })
   }
 
   onSubmit = (evt) => {
     // Use a POST request to send a payload to the server.
+    evt.preventDefault();
   }
 
   render() {
@@ -56,30 +82,30 @@ export default class AppClass extends React.Component {
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">{`Coordinates (${this.state.movesX}, ${this.state.movesY})`}</h3>
+          <h3 id="steps">{`You moved ${this.state.totalSteps} times`}</h3>
         </div>
         <div id="grid">
-          {
-            [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-              <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-                {idx === 4 ? 'B' : null}
+          { 
+            this.state.board.map(idx => (
+              <div key={idx} className={`square${idx === this.state.currentIndex ? ' active' : ''}`}>
+                {idx === this.state.currentIndex ? 'B' : null}
               </div>
             ))
           }
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
           <button id="left">LEFT</button>
-          <button id="up">UP</button>
+          <button onClick={() => this.getNextIndex("up")} id="up">UP</button>
           <button id="right">RIGHT</button>
           <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button onClick={this.reset} id="reset">reset</button>
         </div>
         <form>
-          <input id="email" type="email" placeholder="type email"></input>
+          <input onChange={this.onChange} id="email" type="email" placeholder="type email"></input>
           <input id="submit" type="submit"></input>
         </form>
       </div>
